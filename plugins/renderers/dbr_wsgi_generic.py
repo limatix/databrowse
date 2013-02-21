6009e6d72195949f48ea4e35a3752d72c39b2292
Lesthaeghe, Tyler J <tylerl@iastate.edu>
2013-02-21 15:48:07 -0600

    Adding missing files and updating todo list

---
+++ plugins/renderers/dbr_wsgi_generic.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+###############################################################################
+## Databrowse:  An Extensible Data Management Platform                       ##
+## Copyright (C) 2012 Iowa State University                                  ##
+##                                                                           ##
+## This program is free software: you can redistribute it and/or modify      ##
+## it under the terms of the GNU General Public License as published by      ##
+## the Free Software Foundation, either version 3 of the License, or         ##
+## (at your option) any later version.                                       ##
+##                                                                           ##
+## This program is distributed in the hope that it will be useful,           ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of            ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             ##
+## GNU General Public License for more details.                              ##
+##                                                                           ##
+## You should have received a copy of the GNU General Public License         ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.     ##
+###############################################################################
+""" plugins/renderers/dbr_wsgi_generic.py - Default Handler for WSGI Scripts """
+
+import imp
+import os
+import os.path
+from lxml import etree
+from renderer_support import renderer_class
+
+
+class dbr_wsgi_generic(renderer_class):
+    """ Default Renderer for WSGI Scripts - Simply Passes Everything Off To The Script """
+
+    _savedCWD = None
+    _tempCWD = None
+    _namespace_uri = "http://thermal.cnde.iastate.edu/databrowse/wsgigeneric"
+    _namespace_local = "wsgigeneric"
+
+    def __init__(self, relpath, fullpath, web_support, handler_support, caller, content_mode="raw", style_mode="list", recursion_depth=2):
+        """ Load all of the values provided by initialization """
+        super(dbr_wsgi_generic, self).__init__(relpath, fullpath, web_support, handler_support, caller, content_mode, style_mode)
+        self._savedCWD = os.getcwd()
+        self._tempCWD = os.path.dirname(self._fullpath)
+        etree.register_namespace("wsgigeneric", "http://thermal.cnde.iastate.edu/databrowse/wsgigeneric")
+        pass
+
+    def getContent(self):
+        if self._content_mode is "summary" or self._content_mode is "detailed" or self._content_mode is "title":
+            self.loadStyle()
+            link = self._web_support.siteurl + self._relpath
+            xmlroot = etree.Element('{http://thermal.cnde.iastate.edu/databrowse/wsgigeneric}wsgigeneric', xmlns="http://thermal.cnde.iastate.edu/databrowse/wsgigeneric", name=os.path.basename(self._relpath), href=link)
+            return xmlroot
+        elif self._content_mode is "raw":
+            os.chdir(self._tempCWD)
+            modulename = os.path.splitext(os.path.basename(self._fullpath))[0]
+            module = imp.load_source(modulename, self._fullpath)
+            output = module.application(self._web_support.req.environ, self._web_support.req.start_response)
+            os.chdir(self._savedCWD)
+            self._web_support.req.output_done = True
+            #return output
+            return output
+        else:
+            raise self.RendererException(1102)
+
+    pass
---
+++ support/renderer_support.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+###############################################################################
+## Databrowse:  An Extensible Data Management Platform                       ##
+## Copyright (C) 2012 Iowa State University                                  ##
+##                                                                           ##
+## This program is free software: you can redistribute it and/or modify      ##
+## it under the terms of the GNU General Public License as published by      ##
+## the Free Software Foundation, either version 3 of the License, or         ##
+## (at your option) any later version.                                       ##
+##                                                                           ##
+## This program is distributed in the hope that it will be useful,           ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of            ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             ##
+## GNU General Public License for more details.                              ##
+##                                                                           ##
+## You should have received a copy of the GNU General Public License         ##
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.     ##
+###############################################################################
+""" support/renderer_support.py - Encapsulation Class for Renderer Plugins """
+
+import os.path
+
+
+class renderer_class(object):
+    """ Renderer Plugin Support - Encapsulation Class for Renderer Plugins """
+
+    _relpath = None
+    _fullpath = None
+    _web_support = None
+    _handler_support = None
+    _caller = None
+    _content_mode = None
+    _style_mode = None
+    _dynamic_style = None
+
+    class RendererException(Exception):
+        em = {1101: "Unable to Locate Stylesheet", \
+              1102: "Invalid Content Mode"}
+        pass
+
+    def __init__(self, relpath, fullpath, web_support, handler_support, caller, content_mode="detailed", style_mode="detailed", recursion_depth=2):
+        """ Default Initialization Function """
+        print "In renderer_support.__init__"
+        self._relpath = relpath
+        self._fullpath = fullpath
+        self._web_support = web_support
+        self._handler_support = handler_support
+        self._caller = caller
+        self._content_mode = content_mode
+        self._style_mode = style_mode
+        pass
+
+    def isRaw(self):
+        if self._content_mode is "raw":
+            return True
+        else:
+            return False
+
+    def getStyleMode(self):
+        return self._style_mode
+
+    def getContentMode(self):
+        return self._content_mode
+
+    def getContentMode(self):
+        return self._content_mode
+
+    def loadStyle(self):
+        """ Look In Standard Places For the Appropriate Static Stylesheet """
+        if os.path.isdir(self._fullpath):
+            # Look For Path Like This Where Current Item Is A Directory (Looking Inside Directory):  /fullpath/to/current/item/.databrowse/stylesheets/dbr_plugin_name/detailed.xml
+            filename = os.path.abspath(self._fullpath + '/../.databrowse/stylesheets/' + self.__class__.__name__ + '/' + self._style_mode + '.xml')
+            print "Looking for " + filename
+            if os.path.exists(filename):
+                print "Found!"
+                f = open(filename, 'r')
+                string = f.read()
+                f.close()
+                if self._dynamic_style is not None:
+                    string = string + self._dynamic_style
+                    pass
+                #print 'Calling self._web_support.AddStyle("%s", "%s", "%s")' % (self._namespace_uri, self._content_mode, string)
+                self._web_support.style.AddStyle(self._namespace_uri, string)
+            pass
+        else:
+            # Look For Path Like This Where Current Item Is A File (Looking At Same Directory Level):  /fullpath/to/current/item/.databrowse/stylesheets/dbr_plugin_name/detailed.xml
+            filename = os.path.abspath(os.path.dirname(self._fullpath) + '/.databrowse/stylesheets/' + self.__class__.__name__ + '/' + self._style_mode + '.xml')
+            print "Looking for " + filename
+            if os.path.exists(filename):
+                print "Found!"
+                f = open(filename, 'r')
+                string = f.read()
+                f.close()
+                if self._dynamic_style is not None:
+                    string = string + self._dynamic_style
+                    pass
+                #print 'Calling self._web_support.AddStyle("%s", "%s", "%s")' % (self._namespace_uri, self._content_mode, string)
+                self._web_support.style.AddStyle(self._namespace_uri, string)
+            pass
+
+        # Unable to Find Custom Stylesheets - Revert to App Folder for Static Stylesheet:  /path/to/databrowse/plugins/renderers/stylesheets/dbr_plugin_name/detailed.xml
+        filename = os.path.abspath(self._web_support.rendererpath + '/stylesheets/' + self.__class__.__name__ + '/' + self._style_mode + '.xml')
+        print "Looking for " + filename
+        if os.path.exists(filename):
+            print "Found!"
+            f = open(filename, 'r')
+            string = f.read()
+            f.close()
+            if self._dynamic_style is not None:
+                string = string + self._dynamic_style
+                pass
+            #print 'Calling self._web_support.AddStyle("%s", "%s", "%s")' % (self._namespace_uri, self._content_mode, string)
+            self._web_support.style.AddStyle(self._namespace_uri, string)
+        else:
+            if(hasattr(self, '_style_' + self._style_mode)):
+                print "Found Builtin Style!"
+                string = getattr(self, '_style_' + self._style_mode)
+                if self._dynamic_style is not None:
+                    string = string + self._dynamic_style
+                    pass
+                #print 'Calling self._web_support.AddStyle("%s", "%s", "%s")' % (self._namespace_uri, self._content_mode, string)
+                self._web_support.style.AddStyle(self._namespace_uri, string)
+            else:
+                # Unable to Find Stylesheet Anywhere - Return Error
+                raise self.RendererException(1101)
+            pass
+
+    pass
