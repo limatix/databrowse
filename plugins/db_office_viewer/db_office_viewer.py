b937bd6cbac24843e596a354c1023ca3df199663
Lesthaeghe, Tyler J <tylerl@iastate.edu>
2013-05-10 20:10:14 -0500

    Possible Bugfix

--- plugins/db_office_viewer/db_office_viewer.py
+++ plugins/db_office_viewer/db_office_viewer.py
@@ -116,7 +116,7 @@ class db_office_viewer(renderer_class):
                 else:
                     if not os.path.exists(cachedir):
                         os.makedirs(cachedir)
-                    print " ".join(["/usr/bin/soffice", "--headless", "--convert-to", "pdf", "--outdir", cachedir, self._fullpath])
+                    os.environ["HOME"] = "/var/www/.home"
                     subprocess.call(["/usr/bin/soffice", "--headless", "--convert-to", "pdf", "--outdir", cachedir, self._fullpath])
                     size = os.path.getsize(cachefullpath)
                     f = open(cachefullpath, "rb")
