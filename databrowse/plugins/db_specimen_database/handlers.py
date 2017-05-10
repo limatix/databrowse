1e16c8cbfbe9b9b816e13c3d676c8bc8fe9c1a16
Nate <natescheirer@gmail.com>
2017-05-10 16:25:15 -0500

    listdir recursive access denied bug fix

--- databrowse/plugins/db_specimen_database/handlers.py
+++ databrowse/plugins/db_specimen_database/handlers.py
@@ -42,7 +42,11 @@ import os
 def dbh_directory_specimen(path, contenttype, extension, roottag, nsurl):
     """ Generic Image Directory Handler - Returns directory_image for all directories with more than 50 percent images """
     if contenttype.startswith("inode/directory") or contenttype.startswith("application/x-directory") or contenttype.startswith("directory"):
-        dirlist = os.listdir(path)
+        try:
+            dirlist = os.listdir(path)
+        except OSError:
+            dirlist = []
+            pass
         count = 0
         newlist = [x for x in dirlist if (not ".bak" in x) and (not "_files" in x)]
         for item in newlist:
