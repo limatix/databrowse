c31024052acc88785b42971549722862efbc1316
scheirer <scheirer@iastate.edu>
2018-05-29 12:44:43 -0500

    Reverted setup changes

--- setup.py
+++ setup.py
@@ -38,35 +38,7 @@
 
 import os
 import requirements as r
-import platform
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-from distutils import log # needed for outputting information messages
-from setuptools.command.install_scripts import install_scripts
-
-
-class OverrideInstall(install):
-
-    def run(self):
-        uid, gid = 0, 0
-        mode = 0666
-        install.run(self) # calling install.run(self) insures that everything that happened previously still happens, so the installation does not break!
-        # here we start with doing our overriding and private magic ..
-        for filepath in self.get_outputs():
-            if self.install_scripts in filepath:
-                log.info("Overriding setuptools mode of scripts ...")
-                log.info("Changing ownership of %s to uid:%s gid %s" %
-                         (filepath, uid, gid))
-                os.chown(filepath, uid, gid)
-                log.info("Changing permissions of %s to %s" %
-                         (filepath, oct(mode)))
-                os.chmod(filepath, mode)
-
-
-if platform.system() == "Linux":
-    cmdc = {"install": OverrideInstall}
-else:
-    cmdc = {}
 
 search_dirs = [('databrowse_wsgi', []), ('databrowse_app', [])]
 for search_dir in range(0, len(search_dirs)):
@@ -122,6 +94,5 @@ setup(
         'console_scripts': [
             'databrowse = cefdatabrowse.cefdatabrowse:main'
         ]
-    },
-    cmdclass=cmdc
+    }
 )
